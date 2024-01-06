c29b3878a1433888ba2514259165767a1b5aadfa
Raman Khurana <ramankhurana1986@gmail.com>
2024-01-05 23:21:41 -0600

    added shell scripts for fedformer scan

--- scripts/long_term_forecast/M5/change_template_fedformer.sh
+++ scripts/long_term_forecast/M5/change_template_fedformer.sh
@@ -4,7 +4,13 @@ seq_len_values=(12 24 48 96 192 336)
 
 for seq_len_value in "${seq_len_values[@]}"
 do
-    script_name="run_${model_name}_seq${seq_len_value}.sh"
-    sed "s/SEQ_LEN/${seq_len_value}/g" template_fedformer.sh > $script_name
-    chmod +x $script_name
+    for pred_len_value in "${seq_len_values[@]}"
+    do
+        if [ $pred_len_value -le $seq_len_value ]; then
+            script_name="run_${model_name}_seq${seq_len_value}_pred${pred_len_value}.sh"
+            sed -e "s/SEQ_LEN/${seq_len_value}/g" -e "s/PRED_LEN/${pred_len_value}/g" template_fedformer.sh > $script_name
+            chmod +x $script_name
+        fi
+    done
 done
+
