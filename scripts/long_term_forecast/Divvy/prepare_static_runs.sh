8153fae84b18b97301fa35343546df6a8b5a6167
Raman Khurana <ramankhurana1986@gmail.com>
2024-01-11 09:49:50 -0600

    added all scan scripts for Divvy bikes case

---
+++ scripts/long_term_forecast/Divvy/prepare_static_runs.sh
@@ -0,0 +1,3 @@
+cp DLinear_Divvy.sh run_DLinear_static_scan_seq96_pred96.sh
+cp Autoformer_Divvy.sh run_Autoformer_static_scan_seq96_pred96.sh
+cp FedFormer_Divvy.sh run_FEDformer_static_scan_seq96_pred96.sh
---
+++ scripts/long_term_forecast/Divvy/run_FEDformer_seq24_pred12.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=2
+
+model_name=FEDformer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path /nfs/home/khurana/dataset/Divvy/ \
+  --data_path sales_1000_Random_columns.csv \
+  --model_id Divvy_24_12 \
+  --model $model_name \
+  --data Divvy \
+  --features M \
+  --seq_len 24 \
+  --label_len 6 \
+  --pred_len 12 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 1000 \
+  --dec_in 1000 \
+  --c_out 1000 \
+  --des 'Exp' \
+  --itr 1 \
+  --static NONE
+
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path /nfs/home/khurana/dataset/Divvy/ \
+  --data_path sales_1000_Random_columns.csv \
+  --model_id Divvy_24_12 \
+  --model $model_name \
+  --data Divvy \
+  --features M \
+  --seq_len 24 \
+  --label_len 6 \
+  --pred_len 12 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 1000 \
+  --dec_in 1000 \
+  --c_out 1000 \
+  --des 'Exp' \
+  --itr 1 \
+  --static static4
