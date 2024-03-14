97f3d73d714721dd0ccc8fa6acf5032dacb65c66
Raman Khurana <ramankhurana1986@gmail.com>
2024-03-14 09:40:12 -0500

    results, pdf plots

---
+++ test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=2
+
+model_name=Autoformer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path /nfs/home/khurana/dataset/DivvyBikes/ \
+  --data_path df2021-8-9-10_VAR_nextdayAshrdata.csv \
+  --model_id Divvy_12_12 \
+  --model $model_name \
+  --data Divvy \
+  --features M \
+  --seq_len 12 \
+  --label_len 6 \
+  --pred_len 12 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 1000 \
+  --dec_in 1000 \
+  --c_out 1000 \
+  --des 'Exp' \
+  --itr 1 \
+  --static NONE
+
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path /nfs/home/khurana/dataset/DivvyBikes/ \
+  --data_path df2021-8-9-10_VAR_nextdayAshrdata.csv \
+  --model_id Divvy_12_12 \
+  --model $model_name \
+  --data Divvy \
+  --features M \
+  --seq_len 12 \
+  --label_len 6 \
+  --pred_len 12 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 1000 \
+  --dec_in 1000 \
+  --c_out 1000 \
+  --des 'Exp' \
+  --itr 1 \
+  --static static4
